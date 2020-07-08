a2c2b231cc27dbfc19a4e4d7ae868f3f35d1385f
Duan <1813932878@qq.com>
2020-07-08 18:16:18 +0800

    I'm lazy and don't want to write a description (use English to avoid coding errors)

--- 一键PUSH.bat
+++ 一键PUSH.bat
@@ -2,6 +2,6 @@
 git add .
 ::set /p commitDesc=InputCommitDesc:
 ::git commit -m "%commitDesc%"
-git commit -m "懒得写描述"
+git commit -m "I'm lazy and don't want to write a description (use English to avoid coding errors)"
 git pull
 git push
