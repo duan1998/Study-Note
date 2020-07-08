7efc9d284fc4db72b41c63be9c65f5254df730f6
Duan <1813932878@qq.com>
2020-07-08 18:14:15 +0800

    鎳掑緱鍐欐弿杩?

--- 一键PUSH.bat
+++ 一键PUSH.bat
@@ -1,6 +1,7 @@
 @echo off
 git add .
-set /p commitDesc=InputCommitDesc:
-git commit -m "%commitDesc%"
+::set /p commitDesc=InputCommitDesc:
+::git commit -m "%commitDesc%"
+git commit -m "懒得写描述"
 git pull
 git push
